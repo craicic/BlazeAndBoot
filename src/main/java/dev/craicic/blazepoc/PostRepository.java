9d392d4a98ec288766b5a4b2d0a88a076c455dd4
Craicic <common.hori@protonmail.com>
2023-11-26 11:15:26 +0100

    moved and renamed files

--- src/main/java/dev/craicic/blazepoc/PostRepository.java
+++ src/main/java/dev/craicic/blazepoc/PostRepository.java
@@ -1,5 +1,6 @@
 package dev.craicic.blazepoc;
 
+import dev.craicic.blazepoc.domain.Post;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostRepository extends CrudRepository<Post, Integer>, PostFragmentRepository {
